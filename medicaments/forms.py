8a415c7362afbc2a5a50db79216c483dd57bdb72
Jean Claude NSHIMIRIMANA <nshima82@gmail.com>
2022-09-27 16:29:03 +0000

    favicon

--- medicaments/forms.py
+++ medicaments/forms.py
@@ -1,5 +1,5 @@
 from pyexpat import model
-from tkinter import Widget
+#from tkinter import Widget
 from django.forms import ModelForm,inlineformset_factory,TextInput
 from django import forms
 #from bootstrap_datepicker_plus.widgets import DateTimePickerInput
